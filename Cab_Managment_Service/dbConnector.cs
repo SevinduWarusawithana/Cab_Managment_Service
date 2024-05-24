cfe6a55b94a0f5faf0d0bb499310bd60612701d8
SeviRanWaru <149356832+SeviRanWaru@users.noreply.github.com>
2024-05-24 20:07:05 +0530

    adding dbconnector class

---
+++ Cab_Managment_Service/dbConnector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cab_Managment_Service
+{
+    internal class dbConnector
+    {
+        public string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""D:\APIIT\Sem02 L4\Software Devolopment & Application Modelling 02\cab_system_db\car_management_db.mdf"";Integrated Security=True;Connect Timeout=30;Encrypt=False";
+
+
+    }
+}
