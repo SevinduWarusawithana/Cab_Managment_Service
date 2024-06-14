f9451c12fae6ddfcd46c458dcb18529330b312ea
SeviRanWaru <149356832+SeviRanWaru@users.noreply.github.com>
2024-06-14 17:49:32 +0530

    rename

--- Cab_Managment_Service/CarManagementScreen.cs
+++ Cab_Managment_Service/CarManagementScreen.cs
@@ -15,18 +15,12 @@ namespace Cab_Managment_Service
 {
     public partial class CarManagementScreen : Form
     {
-        /*public CarManagementScreen()
-        {
-            InitializeComponent();
-        }*/
-
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
 
         }
 
         
-
         private void addNewCarBtn_Click(object sender, EventArgs e)
         {
             if (addingCarNameTxt.Text == "" || addingCarPlateNumberTxt.Text == "" || addingCarAvailablityCombo.Text == "")
